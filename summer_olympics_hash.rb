ddfa541765980f6bd2741303c20971cb93923f6a
christinaalenda <clenda041@gmail.com>
2018-08-08 19:53:27 +0000

    Done.

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,16 +1,19 @@
 def create_olympics_hash
-  {Sydney; "2000", Athens; "2004", Beijing; "2008", London; "2012"}
+  {Sydney; "2000", 
+  Athens; "2004", 
+  Beijing; "2008", 
+  London; "2012"}
 end
 
 def add_a_key_value_pair
 new_hash = create_olympics_hash
-new_hash[;Atlanta] = "1996"
+new_hash[:Atlanta] = "1996"
 new_hash
 end
 
 def iterate_through_hash
-add_a_key_value_pair.each {|key,value|}
-puts "The #{value} summer olympics took place in #{key}."
+add_a_key_value_pair.each {|key,value|
+puts "The #{key} summer olympics took place in #{value}."
 end
 
 def iterate_through_keys
