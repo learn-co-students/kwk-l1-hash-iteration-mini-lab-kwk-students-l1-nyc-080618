70c09c0edb5bbebf6a860e897d75fa9eb15e2cd9
kazinur <kazinur@mybihs.org>
2018-08-08 18:05:44 +0000

    Done.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ### The Summer Olympics
-<img src="https://s3.amazonaws.com/after-school-assets/sydney.jpg" width="300px" align="right" hspace="10"> The Summer Olympics take place every four years in different location around the world. Using the data from the table below, create a hash `summer_olympics`, using the city as the hash's key and the year as the hash's value. Use `:symbols` instead of string for the keys.
+<img src="https://s3.amazonaws.com/after-school-assets/sydney.jpg" width="300px" align="right" hspace="10"> The Summer Olympics take place every four years in different location around the world. Using the data from the table below, create a hash `summer_olympics`se `:, using the city as the hash's key and the year as the hash's value. Usymbols` instead of string for the keys.
 
 
 |Place          | Year      |
--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,18 +1,14 @@
+olympics_hash = {:Sydney => "2000", :Athens => "2004", :Beijing => "2008", :London => "2012"} 
 
-def create_olympics_hash
-  # Implement this method so that it returns a hash with the data provided on README.md
-end
+olympics_hash[:Atlanta] = "1996"
 
-def add_a_key_value_pair
-  # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
-end
+puts olympics_hash
 
-def iterate_through_hash
-  # Implement this method so that it iterates over the hash created in add_a_key_value_pair
-  # and puts each key value pair within the phrase "The _____ summer olympics took place in _____."
-end
+olympics_hash.each do |cities,year|
+  puts "The #{cities} summer olympics took place in #{year}"
 
-def iterate_through_keys
-  # Implement this method so that it converts the keys of the hash created in add_a_key_value_pair
-  # into uppercased Strings, creates a new array with these values, and then puts each upcase key in the CLI
+ 
+olympics_hash.each do |cities|
+  puts "#{cities}".upcase 
 end
+end 
