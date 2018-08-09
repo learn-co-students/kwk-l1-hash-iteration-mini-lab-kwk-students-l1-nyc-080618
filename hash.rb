9bb5100b6bfc302d192a2a057c2700f6a6580dc4
rochellesagh <rsrochelli@gmail.com>
2018-08-09 17:41:42 +0000

    Done.

--- hash.rb
+++ hash.rb
@@ -1,12 +1,12 @@
-# shopping_hash = {"apple"=> 1, "banana"=> 2, "carrot"=> 3} 
+shopping_hash = {"apple"=> 1, "banana"=> 2, "carrot"=> 3} 
 puts shopping_hash
 
 # Food: apple, Price:1 
 # Food: banana, Price:2 
-#Food carrot, Price 3
+# Food carrot, Price 3
 
 shopping_hash.each do |food, price| 
-  
+  puts "Food: #{food}, Price: #{price}"
   #loop 1: food = apple, price = 1 
   #loop 2: food = banana, price = 2 
   #loop 3: food = carrot, price = 3
