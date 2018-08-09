c05b191efb66c22aabcca1efbbec718b30d8c73b
rochellesagh <rsrochelli@gmail.com>
2018-08-09 18:26:53 +0000

    Done.

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,27 +1,45 @@
 def create_olympics_hash
+
   # Implement this method so that it returns a hash with the data provided on README.md
   
-  summer_olympics_hash = { 
-|Place          | Year      |
-| ------------- |:---------:|
-| Atlanta       | 1996      |
-| Sydney        | 2000      |
-| Athens        | 2004      |
-| Beijing       | 2008      |
-| London        | 2012      | } 
+  create_olympics_hash = {"Atlanta"=> 1996, "Sydney"=> 2000, "Athens"=> 2004, "Beijing"=> 2008, "London"=> 2012}
+  
+  
+# |Place          | Year      |
+# | ------------- |:---------:|
+# | Atlanta       | 1996      |
+# | Sydney        | 2000      |
+# | Athens        | 2004      |
+# | Beijing       | 2008      |
+# | London        | 2012      | } 
 
-puts summer_olympics_hash
+create_olympics_hash.each do |place, year| 
+
+  puts "Place: #{place}, Year: #{year}"
+
 
 end
+return create_olympics_hash
+end 
 
-def add_a_key_value_pair
-  # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
-end
+# puts create_olympics
+
+# def add_a_key_value_pair
+#   # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
+# end
 
 def iterate_through_hash
   # Implement this method so that it iterates over the hash created in add_a_key_value_pair
-  # and puts each key value pair within the phrase "The _____ summer olympics took place in _____."
-end
+  # and puts each key value pair within the phrase "The _____ summer olympics took place in _____
+  
+   iterate_through_hash = {"Atlanta"=> 1996, "Sydney"=> 2000, "Athens"=> 2004, "Beijing"=> 2008, "London"=> 2012}
+   
+   iterate_through_hash.each do |place, year| 
+     puts "The #{place} summer olympics took place in #{year}."
+                            end
+end 
+
+iterate_through_hash
 
 def iterate_through_keys
   # Implement this method so that it converts the keys of the hash created in add_a_key_value_pair
